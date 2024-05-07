c49e7667b0826a1ab4c817fdf582ddf982ab9a4c
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-05-07 01:33:54 -0700

    fixed navigation responsive it used to be wierd not size right

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
 const burgerMenu = document.querySelector(".burgerIcon");
 const navigation = document.querySelector(".navburger");
 burgerMenu.addEventListener("click", () => {
   navigation.classList.toggle("navigationActive");
 });
-
